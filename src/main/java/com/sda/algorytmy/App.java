4e8727327836387bbba1a412e93f160b8a137f99
Michal Stusik <x5htpx@gmail.com>
2019-05-12 14:32:23 +0100

    dfr

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,19 @@
+package com.sda.algorytmy;
+
+/**
+ Zadanie
+ Dana jest tablica: a = {4, 1, 6, 1, 1, 4, 3, 3, 1}.
+ Proszę zgodnie ze schematem blokowym przeprowadzić zliczanie liczby: 1.
+ 1) Na kartce rozpisać wszystkie kroki.
+ 2) Zaimplementować kod
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int [] a = {4,1,6,1,1,4,3,3,1};
+
+
+    }
+}
