900938bd7a7273d1e3927f1018f16572bd6e997f
Michal Stusik <x5htpx@gmail.com>
2019-05-12 14:39:51 +0100

    asdfg

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,5 +1,8 @@
 package com.sda.algorytmy;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  Zadanie
  Dana jest tablica: a = {4, 1, 6, 1, 1, 4, 3, 3, 1}.
@@ -12,8 +15,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        int [] a = {4,1,6,1,1,4,3,3,1};
+        int [] tab = {4,1,6,1,1,4,3,3,1};
+        int zliczanie = 0;
 
+        System.out.println(Arrays.toString(tab));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Liczba, ktora chcesz zliczyc?");
+
+        int wartosc = scanner.nextInt();
+
+
+
+        for(int i = 0; i < tab.length; i++){
+
+            if (tab[i] == wartosc){
+
+                zliczanie++;
+
+            }
+
+        }
+
+        System.out.println("Liczba " + wartosc + "wystepuje " + zliczanie + "razy.");
 
     }
+
 }
+
+
+
